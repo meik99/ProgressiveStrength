3d83a5d7474fb71b33b53f06a5202fcb30b6ab11
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2018-03-22 05:39:37 +0100

    Added Manage Exercise Button and Activity

--- app/src/main/java/com/rynkbit/progressivestrength/MainActivity.java
+++ app/src/main/java/com/rynkbit/progressivestrength/MainActivity.java
@@ -1,14 +1,33 @@
 package com.rynkbit.progressivestrength;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabAddDay;
+    private RecyclerView listDays;
+    private Button btnManageExercises;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fabAddDay = findViewById(R.id.fabAddDays);
+        listDays = findViewById(R.id.listDays);
+        btnManageExercises = findViewById(R.id.btnManageExercises);
+
+        btnManageExercises.setOnClickListener((view) -> {
+            startActivity(
+                    new Intent(view.getContext(), MainActivity.class)
+            );
+        });
     }
 }
---
+++ app/src/main/java/com/rynkbit/progressivestrength/exercise/MananageExercisesActivity.java
@@ -0,0 +1,15 @@
+package com.rynkbit.progressivestrength.exercise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rynkbit.progressivestrength.R;
+
+public class MananageExercisesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mananage_exercises);
+    }
+}
